8a04b1cb7e014ad8d2f960a1b8ac1f9dbd6121f2
PhotnixtBlaze <Phoenixtblaze@gmail.com>
2025-12-04 15:47:25 +0530

    small update to about files.

--- SaberSurgeon/Properties/AssemblyInfo.cs
+++ SaberSurgeon/Properties/AssemblyInfo.cs
@@ -6,11 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SaberSurgeon")]
-[assembly: AssemblyDescription("A BeatSaber Mod that does mod stuff I guess")]
+[assembly: AssemblyDescription("Twitch chat-driven Beat Saber modifiers (SaberSurgeon).")]
 [assembly: AssemblyConfiguration("SaberSurgeon")]
-[assembly: AssemblyCompany("SaberSurgeon")]
+[assembly: AssemblyCompany("Phoenixblaze0")]
 [assembly: AssemblyProduct("SaberSurgeon")]
-[assembly: AssemblyCopyright("Copyright © PhoenixBlaze  2025")]
+[assembly: AssemblyCopyright(
+    "Copyright © PhoenixBlaze0 2025 - "
+    + "All rights reserved. No modifications without explicit permission.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
